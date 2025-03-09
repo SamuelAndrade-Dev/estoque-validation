5905dd3f8dcd726414e79ff8729dd6ce6530a903
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-03-09 12:23:10 -0300

    funcao para o captcha

--- login.js
+++ login.js
@@ -1,10 +1,17 @@
 function logar() {
   let login = document.getElementById("login").value;
   let senha = document.getElementById("senha").value;
+  let captcha = document.getElementById("captcha").value;
 
   if (login == "samuelppa" && senha == "samuel") {
     location.href = "estoque.html";
   } else {
     alert("Usuario ou senha incorreto");
+  }
+
+  if (captcha == "checked") {
+    location.href = "estoque.html";
+  } else {
+    alert("Captcha inválido");
   }
 }
